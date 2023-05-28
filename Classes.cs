ef26268819205846eff7b931db5a06e8e31685a3
Mahmoud Taha <132286980+MahmoudMonis@users.noreply.github.com>
2023-05-28 04:13:48 +0300

    Generics and Collections in C#

---
+++ Classes.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Readings;
+
+class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Employee(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using Readings;
+
+class Program
+{
+    private const string Name = "Mahmoud Taha";
+
+    static void Main(string[] args)
+    {
+        List<int> intList = new List<int>();
+        List<string> stringList = new List<string>();
+        List<Employee> employeeList = new List<Employee>();
+
+        // Add items
+        intList.Add(10);
+        intList.Add(20);
+
+        stringList.Add("Mahmoud");
+        stringList.Add("cristiano");
+
+        employeeList.Add(new Employee(1, "Mahmoud Taha"));
+        employeeList.Add(new Employee(2, "cristiano ronaldo"));
+
+        // Get items
+        Console.WriteLine("Int List:");
+        foreach (int number in intList)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine("\nString List:");
+        foreach (string name in stringList)
+        {
+            Console.WriteLine(name);
+        }
+
+        Console.WriteLine("\nEmployee List:");
+        foreach (Employee employee in employeeList)
+        {
+            Console.WriteLine($"ID: {employee.Id}, Name: {employee.Name}");
+        }
+
+        // Delete items
+        intList.Remove(20);
+        stringList.Remove("Mahmoud");
+        employeeList.RemoveAll(e => e.Id == 2);
+
+        Console.WriteLine("\nAfter deletion:");
+
+        Console.WriteLine("Int List:");
+        foreach (int number in intList)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine("\nString List:");
+        foreach (string name in stringList)
+        {
+            Console.WriteLine(name);
+        }
+
+        Console.WriteLine("\nEmployee List:");
+        foreach (Employee employee in employeeList)
+        {
+            Console.WriteLine($"ID: {employee.Id}, Name: {employee.Name}");
+        }
+
+        // Clear all items
+        intList.Clear();
+        stringList.Clear();
+        employeeList.Clear();
+
+        Console.WriteLine("\nAfter clearing all items:");
+
+        Console.WriteLine("Int List:");
+        foreach (int number in intList)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine("\nString List:");
+        foreach (string name in stringList)
+        {
+            Console.WriteLine(name);
+        }
+
+        Console.WriteLine("\nEmployee List:");
+        foreach (Employee employee in employeeList)
+        {
+            Console.WriteLine($"ID: {employee.Id}, Name: {employee.Name}");
+        }
+    }
+}
